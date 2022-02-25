d593825b821e2633a98e6dd1904d62bfc83c3dd5
Aaron Prowse <aaron.prowse@live.co.uk>
2022-02-25 21:36:32 +0000

    Base Files Setup

---
+++ wp-content/plugins/http-post/http-post.php
@@ -0,0 +1,10 @@
+<?php
+   /*
+   Plugin Name: HTTP-Post
+   Plugin URI: https://github.com/aaronprowse/Reason-Digital
+   description: Custom Plugin for HTTP Post requests when content is modified.
+   Version: 1.0
+   Author: Aaron Prowse
+   Author URI: https://github.com/aaronprowse/Reason-Digital
+   */
+?>
